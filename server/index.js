fc2864be7b5ac43f1136f151471831d516111392
JevF1982 <40211448+JevF1982@users.noreply.github.com>
2020-08-13 20:53:16 +0200

    images fix

--- server/index.js
+++ server/index.js
@@ -35,16 +35,16 @@ app.use("/api/product", require("./routes/product"));
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
-// app.use("/uploads", express.static("server/uploads"));
+
 app.use("/uploads", express.static("server/uploads"));
-app.use("/uploads", express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "server/uploads")));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   // All the javascript and css files will be read and served from this folder
   app.use("/uploads", express.static("server/uploads"));
-  app.use("/uploads", express.static(path.join(__dirname, "public")));
+  app.use("/uploads", express.static(path.join(__dirname, "server/uploads")));
   app.use(express.static("client/build"));
 
   // index.html for all page routes    html or routing and naviagtion
