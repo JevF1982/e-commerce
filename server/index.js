0ca07f90e7d9d0ec50f90bda083508ad5ab532e1
JevF1982 <40211448+JevF1982@users.noreply.github.com>
2020-08-13 14:22:08 +0200

    drawer fix

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const config = require("./config/key");
 
 const mongoose = require("mongoose");
 const connect = mongoose
-  .connect(config.mongoURI || mongoURI, {
+  .connect(config.mongoURI , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
