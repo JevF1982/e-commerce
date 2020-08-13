9a77b3942f39a30b90e7cc854119b231c3aba517
JevF1982 <40211448+JevF1982@users.noreply.github.com>
2020-08-13 19:29:31 +0200

    images fix

--- server/routes/product.js
+++ server/routes/product.js
@@ -8,7 +8,7 @@ const { Product } = require("../models/Product.js");
 
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, `C:\\Users\\faust\\Desktop\\webdev\\E-commerce\\uploads`);
+    cb(null, `server/uploads`);
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}_${file.originalname}`);
