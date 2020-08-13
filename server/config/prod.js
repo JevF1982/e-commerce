26f04824a43492af151c85002e1090ce61a30521
JevF1982 <40211448+JevF1982@users.noreply.github.com>
2020-08-13 13:33:34 +0200

    URI fix

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,5 @@
 module.exports = {
-    mongoURI:process.env.MONGO_URI
-}
+  mongoURI:
+    process.env.MONGO_URI ||
+    "mongodb+srv://Faust:werkgewoon@mernsoppingapp.ymbxm.gcp.mongodb.net/MernSoppingApp?retryWrites=true&w=majority",
+};
