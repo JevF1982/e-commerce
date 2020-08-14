c3d79ede3a7f7d2056a0ee3546c23c8784c429ad
JevF1982 <40211448+JevF1982@users.noreply.github.com>
2020-08-14 18:09:14 +0200

    image fix

--- server/index.js
+++ server/index.js
@@ -31,11 +31,10 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/product", require("./routes/product"));
 
 app.use(express.static("./server/uploads"));
-app.use("/server/uploads", express.static(path.join(__dirname, "public")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/server/uploads", express.static(path.join(__dirname, "public")));
   app.use(express.static("client/build"));
+  app.use(express.static("./server/uploads"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
